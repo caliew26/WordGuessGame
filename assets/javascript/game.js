72a3cff93f7dd1ad1e2fae341a4bcf011ee7cc7e
Adventure 2019 <caliew26@gmail.com>
2019-03-26 10:45:19 -0400

    added font on css started game.js documentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,29 @@
-<script text="text/javascript">
-    
+//random word pulled from list I created
+var words = ["mickey mouse", "walt disney", "anaheim", "trains", "princess", "orlando", "tower of terror", "epcot", "magic", "fireworks", "parades", "fantasia"];
+
+//max number of guesses allowed
+//will need to keep track of them; reduce (i--) by 1 everytime a letter is guessed
+var numGuess = 13;
+
+var numGuessRemain = 0;
+
+//list of all guessed letters
+var lettersGuessed = [];
+
+//an underscore listed for the length of each word as it is generated
+var ansWordArray = [];
+
+//keep track of all the wins, user guessed the words (would be within the alloted number of guesses)
+var numWins = 0;
+
+//keep track of all the loses, user didn't get the word in the number of allowed guesses -->
+var numLosses = 0;
+
 //docment onkeyup what letter the user guesses and make it lowercase
 document.onkeyup = event.key.toLowerCase();
 
-//random word pulled from list I created
-var words = ["mickey mouse", "walt disney", "anaheim", "trains", "princess", "orlando", "tower of terror", "epcot", "magic", "fireworks", "parades", "fantasia"]
 
-//user will have 10 guesses - will need to track them
-//keep a list of all guessed letters
+
+
 //as the user guesses the letter correctly; reveal them in the correct place
-//an underscore listed for the length of each word as it is generated
 //
-//keep track of all the wins, user guessed the words (would be within the alloted number of guesses)
-//keep track of all the loses, user didn't get the word in the number of allowed guesses -->
-    var wins = 
-
-
-</script>
