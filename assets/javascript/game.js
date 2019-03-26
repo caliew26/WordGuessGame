aa61917ddf6c032c260882b6d535b2edb1ac8017
Adventure 2019 <caliew26@gmail.com>
2019-03-26 12:26:08 -0400

    added a bunch of var (delared) values in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,58 @@
-//random word pulled from list I created
-var words = ["mickey mouse", "walt disney", "anaheim", "trains", "princess", "orlando", "tower of terror", "epcot", "magic", "fireworks", "parades", "fantasia"];
+//all declares are at the top of this script
+var words = ["mickey mouse", "walt disney", "anaheim", "trains", "princess", "orlando", "tower of terror", "epcot", "magic", "fireworks", "parades", "fantasia"]; ////need to declare the words to use to pay the game
+var maxNumGuess = 13; //declare the max number of guesses allowed 
+var remainingGuesses = 0; //how many tries the user has left
+var currentWordIndex; //index of the currend word in the array
+var wordToGuess; //current word randomly chosen to guess
+var lettersGuessed = []; //array of letters guessed
+var numWins = 0; //keep track of all the wins,
+var numLosses = 0; //keep track of all the loses
 
-//max number of guesses allowed
-//will need to keep track of them; reduce (i--) by 1 everytime a letter is guessed
-var numGuess = 13;
 
-var numGuessRemain = 0;
+//all functions for this script are below here
 
-//list of all guessed letters
-var lettersGuessed = [];
+//need to start the game with everything reset
+function resetGame() {
 
-//an underscore listed for the length of each word as it is generated
-var ansWordArray = [];
+    for (var i = 0; i < words[wordToGuess].length; i++) {
 
-//keep track of all the wins, user guessed the words (would be within the alloted number of guesses)
-var numWins = 0;
+    }
 
-//keep track of all the loses, user didn't get the word in the number of allowed guesses -->
-var numLosses = 0;
+    remainingGuesses = maxNumGuess;
+    gameStarted = false;
+
+
+    currentWordIndex = words[Math.floor(Math.random() * words.length)].toLowerCase();  //random word picked to play
+
+    lettersGuessed = [];
+    wordToGuess = [];
+
+    //this will select the random word and output an underscore for the length of the word
+    for (var i = 0; i <words[currentWordIndex].length; i++); {
+        wordToGuess.push("_") ;
+    }
+
+    updateDisplay();
+
+}
 
 //docment onkeyup what letter the user guesses and make it lowercase
-document.onkeyup = event.key.toLowerCase();
+document.onkeyup = function(event) {
+event.key.toLowerCase();
+}
+
+//run a loop function reducing the number by 1
+for (var i = 0; i < -1; i--) {
+    numGuessRemain [i];
+}
 
 
+function myFunction() {
+  var x = document.getElementById("alreadyguessed").value;
+}
 
 
 //as the user guesses the letter correctly; reveal them in the correct place
-//
+
+
+}
